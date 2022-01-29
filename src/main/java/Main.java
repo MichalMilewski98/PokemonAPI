968c3045caea5dc2da7a6e0f849e3047463432e1
Michal Milewski <michal.milewski98@gmail.com>
2022-01-29 20:49:56 +0100

    Add user input handling, make multiplierMap static final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,31 @@
+import rest.PokemonRestService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        String attackType;
+        List<String> destPokemonType;
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Input: ");
+        String input = bf.readLine();
+
+        String[] inputs = input.split(" -> ");
+
+        attackType = inputs[0];
+        destPokemonType = Arrays.asList(inputs[1].split(" "));
+
+        PokemonRestService pokemonRestService = new PokemonRestService();
+        PokemonCalculator pokemonCalculator = new PokemonCalculator();
+        System.out.println(pokemonCalculator.calculateEffectiveness(
+                pokemonRestService.getPokemonType(attackType).getBody(), destPokemonType));
+
+        bf.close();
     }
 }
--- src/main/java/PokemonCalculator.java
+++ src/main/java/PokemonCalculator.java
@@ -2,33 +2,34 @@ import kong.unirest.JsonNode;
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PokemonCalculator {
 
-    public void calculateDamage(JsonNode jsonData, String defenderName) {
-        HashMap<String, String> multiplierMap = new HashMap<>();
-        try {
-            multiplierMap.put("half_damage_to", "0.5x");
-            multiplierMap.put("double_damage_to", "2x");
-            multiplierMap.put("no_to", "0x");
+    private static final Map<String, Double> multiplierMap = Map.of(
+            "no_damage_to", (double) 0,
+            "half_damage_to", 0.5,
+            "double_damage_to", 2.0
+    );
 
-            JSONObject json = jsonData.getObject();
-            JSONObject damageRelations = json.getJSONObject("damage_relations");
-            for (String damage : damageRelations.keySet()) {
-                if (damage.contains("_to")) {
-                    JSONArray attackerDamageRelations = damageRelations.getJSONArray(damage);
+    public double calculateEffectiveness(JsonNode jsonData, List<String> defenderNames) {
+        double result = 1;
+        JSONObject json = jsonData.getObject();
+        JSONObject damageRelations = json.getJSONObject("damage_relations");
+        for (String damage : damageRelations.keySet()) {
+            if (damage.contains("_to")) {
+                JSONArray attackerDamageRelations = damageRelations.getJSONArray(damage);
+                for(String defenderName: defenderNames) {
                     for (int i = 0; i <= attackerDamageRelations.length() - 1; i++) {
                         JSONObject defenderType = (JSONObject) attackerDamageRelations.get(i);
                         if (defenderType.get("name").toString().equalsIgnoreCase(defenderName)) {
-                            System.out.println(multiplierMap.get(damage));
-                            break;
+                            result *= multiplierMap.get(damage);
                         }
                     }
                 }
             }
-        } catch (Exception e) {
-
         }
+        return result;
     }
 }
